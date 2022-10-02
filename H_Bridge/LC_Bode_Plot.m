442c98189529d17edb7cded856e5b29c64bd1d24
Mikestriken <michael.john.marais379@gmail.com>
2022-10-01 23:24:55 -0400

    Added main content

---
+++ H_Bridge/LC_Bode_Plot.m
@@ -0,0 +1,15 @@
+L=2.5e-3
+C=2e-6
+R=2.2
+
+% LC Network Transfer
+lCTransfer = tf([1], [L*C 0 1])
+
+% RLC Network Transfer
+rLCTransfer = tf([1], [L*C L/R 1])
+
+% bode(transfer)
+grid on
+
+ bode(rLCTransfer)
+grid on
